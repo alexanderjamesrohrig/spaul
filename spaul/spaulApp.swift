3a25b9b361ed560272b891159c0cfd3b72eea219
Alexander Rohrig <me@alexanderrohrig.com>
2023-04-30 11:22:55 -0500

    Initial Commit

---
+++ spaul/spaulApp.swift
@@ -0,0 +1,17 @@
+//
+//  spaulApp.swift
+//  spaul
+//
+//  Created by Alexander Rohrig on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct spaulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
